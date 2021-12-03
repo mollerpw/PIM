fd6b942def8ee95199e3213d9750332381e46413
theokinell02 <theo@kinell.nu>
2021-12-03 11:11:44 +0100

    sout hello world

--- src/server/Main.java
+++ src/server/Main.java
@@ -2,7 +2,7 @@ package server;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hej");
+        System.out.println("hello world");
         System.out.println("test");
     }
     int lol = 5;
