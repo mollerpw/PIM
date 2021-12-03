b49b522bab510156e3743a12a658329d20e4da97
mollerpw <89844600+mollerpw@users.noreply.github.com>
2021-12-03 10:47:50 +0100

    Main update test

--- src/server/Main.java
+++ src/server/Main.java
@@ -3,6 +3,7 @@ package server;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hej");
-        System.out.println("axels branch");
+        
+
     }
 }
