d806c3354f4151433af02e22ae9eed4fb909a083
theokinell02 <theo@kinell.nu>
2021-12-06 11:08:59 +0100

    crud metoder i databas classen

--- src/server/Database.java
+++ src/server/Database.java
@@ -13,7 +13,6 @@ public class Database {
             conn = DriverManager.getConnection("jdbc:sqlite:PIM.db");
             PreparedStatement stmt = conn.prepareStatement("SELECT * FROM notes");
             ResultSet rs = stmt.executeQuery();
-            conn.close();
             System.out.println(rs.next());
         } catch (SQLException e) {
             e.printStackTrace();
@@ -23,35 +22,26 @@ public class Database {
     public String Read(String noteName){
         String output = "";
         try {
-            Connection conn = DriverManager.getConnection("jdbc:sqlite:PIM.db");
             PreparedStatement stmt = conn.prepareStatement("SELECT content FROM notes WHERE name='" + noteName + "';");
             ResultSet rs = stmt.executeQuery();
-            while (rs.next()){
-                output += rs.getString("content") + ", ";
-            }
-            conn.close();
+            output = rs.getString("content") + ", ";
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
         return output;
     }
-    public static void Write(String noteName, String content){
+    public void Write(String noteName, String content){
         try {
-            Connection conn = DriverManager.getConnection("jdbc:sqlite:PIM.db");
             PreparedStatement stmt = conn.prepareStatement("UPDATE notes SET content='" + content + "', timestamp ='" + LocalDateTime.now() + "' WHERE name='" + noteName + "';");
             stmt.execute();
-            conn.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
-    public static void Create(String noteName, String folder){
+    public void Create(String noteName, String folder){
         try {
-            Connection conn = DriverManager.getConnection("jdbc:sqlite:PIM.db");
             PreparedStatement stmt = conn.prepareStatement("INSERT INTO notes (name, folder, timestamp) VALUES ('" + noteName + "', '" + folder + "', '" + LocalDateTime.now() + "');");
             stmt.execute();
-            conn.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
