839dee93d5a424f348642e8ac5abb82ce75ddf4f
mollerpw <89844600+mollerpw@users.noreply.github.com>
2021-12-07 14:45:12 +0100

    Folder class.java

---
+++ src/server/Folder.java
@@ -0,0 +1,29 @@
+package server;
+
+public class Folder {
+    private String name;
+    private String timestamp;
+    public Folder() {
+    }
+
+    public Folder(String name, String timestamp) {
+        this.name = name;
+        this.name = timestamp;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+}
