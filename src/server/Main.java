ad2e34f829142389a307ad3de86371773f0414c2
Tajiana Odero <61429156+Taj-Ode@users.noreply.github.com>
2021-12-07 11:26:46 +0100

    Added old 0.3.5 java-express jar file

--- src/server/Main.java
+++ src/server/Main.java
@@ -1,6 +1,10 @@
 package server;
 
 import express.Express;
+import express.middleware.Middleware;
+
+import java.io.IOException;
+import java.nio.file.Paths;
 
 import java.nio.file.Paths;
 
@@ -11,14 +15,14 @@ public class Main {
         Express app = new Express();
 
         app.get("/notes", (req, res) -> {
-            Note notes = req.body(Note.class);
+            Note notes = (Note) req.getBody(Note.class);
             String noteName = notes.getName();
             String content = notes.getContent();
             db.Write(noteName, content);
         });
 
         app.put("/notes", (req, res) -> {
-           Note notes = req.body(Note.class);
+           Note notes = (Note) req.getBody(Note.class);
            String noteName = notes.getName();
            String outPut = db.Read(noteName);
            res.json(outPut);
@@ -26,36 +30,46 @@ public class Main {
         });
 
         app.post("/notes", (req, res) -> {
-            Note notes = req.body(Note.class);
+            Note notes = (Note) req.getBody(Note.class);
             String noteName = notes.getName();
             String folder = notes.getFolder();
             db.Create(noteName, folder);
         });
 
 
+<<<<<<< Updated upstream
         app.post("/folders", (req, res) -> {
         Note notes = req.body(Note.class);
+=======
+        app.get("/folders", (req, res) -> {
+        Note notes = (Note) req.getBody(Note.class);
+>>>>>>> Stashed changes
         String folder = notes.getFolder();
-
         res.json(db.createFolder(folder));
         });
 
 
         app.delete("/folders", (req, res) -> {
-            Note notes = req.body(Note.class);
+            Note notes = (Note) req.getBody(Note.class);
             String folder = notes.getFolder();
             db.deleteFolder(folder);
         });
 
 
         app.delete("/notes", (req, res) -> {
-            Note notes = req.body(Note.class);
+            Note notes = (Note) req.getBody(Note.class);
             String note = notes.getName();
             db.deleteNote(note);
         });
 
-
         app.listen(2000);
         System.out.println("Server started on port 2000");
+
+        try {
+            app.use(Middleware.statics(Paths.get("src/www").toString()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
