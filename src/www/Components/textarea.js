87768bcd063a12c00cee17697004eb847656fcad
mollerpw <89844600+mollerpw@users.noreply.github.com>
2021-12-10 11:22:18 +0100

    Textarea download file update

--- src/www/Components/textarea.js
+++ src/www/Components/textarea.js
@@ -10,7 +10,9 @@ function renderWritingField() {
 
     if (currentNoteName.content == null){
         document.querySelector("#writingfield").innerHTML = `<textarea id=textArea placeholder= "Write here..."></textarea>`
-    } else {
-        document.querySelector("#writingfield").innerHTML = `<img id="img" src=${currentNoteName.imageURL}></img><a href="${currentNoteName.uploadFile}" download>${currentNoteName.uploadFile}</a><textarea id=textArea placeholder= "Write here...">${currentNoteName.content}</textarea>`
+    } else if (currentNoteName.uploadFile == null || currentNoteName.uploadFile == ""){
+        document.querySelector("#writingfield").innerHTML = `<img id="img" src=${currentNoteName.imageURL}></img><textarea id=textArea placeholder= "Write here...">${currentNoteName.content}</textarea>`
+    }else {
+        document.querySelector("#writingfield").innerHTML = `<img id="img" src=${currentNoteName.imageURL}></img><a href="${currentNoteName.uploadFile}" download>Download ${currentNoteName.uploadFile}</a><textarea id=textArea placeholder= "Write here...">${currentNoteName.content}</textarea>`
     }
 }
