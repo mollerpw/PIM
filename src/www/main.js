1cc9cd51e9b1b0ee39b044e6c75520afddd91be5
theokinell02 <theo@kinell.nu>
2021-12-14 15:08:03 +0100

    Update main.js

--- src/www/main.js
+++ src/www/main.js
@@ -1,37 +1,3 @@
 document.querySelector('header').innerHTML = "<h1>PIM</h1>";
 
 renderFolder();
-
-/* 
-onhashchange = changePage;
-
-changePage();
-
-function changePage(){
-
-    let page = location.hash.replace('#','');
-
-    switch(page){
-        case "menu":
-            let menu = new Menu();
-            document.querySelector('main').innerHTML = menu.render();
-        break;
-
-        case "contact":
-            let contact = new Contact();
-            document.querySelector('main').innerHTML = contact.render();
-        break;
-
-        case "about":
-            let about = new About();
-            document.querySelector('main').innerHTML = about.render();
-        break;
-
-        default:
-            //tanke är om det går fel i routing så kommer man till home page
-            let home = new Home();
-            document.querySelector('main').innerHTML = home.render();
-        break;
-
-    }
-}*/
